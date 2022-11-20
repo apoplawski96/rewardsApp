d9afebb93b243ffc970e80e55763431942f54fac
arturmacbookpro <a.poplawski96@gmail.com>
2022-11-20 18:10:59 +0100

    chore: common-utils module cleanup

--- common/common-util/src/main/java/com/futuremind/loyaltyrewards/common/util/test/TestDispatcherProviderFactory.kt
+++ common/common-util/src/main/java/com/futuremind/loyaltyrewards/common/util/test/TestDispatcherProviderFactory.kt
@@ -2,13 +2,16 @@ package com.futuremind.loyaltyrewards.common.util.test
 
 import com.futuremind.loyaltyrewards.common.util.coroutines.DispatcherProvider
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Suppress("FunctionName")
 fun TestDispatcherProvider(
     dispatcher: CoroutineDispatcher = UnconfinedTestDispatcher()
 ): DispatcherProvider = MTestDispatcherProvider(dispatcher)
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class MTestDispatcherProvider(
     private val dispatcher: CoroutineDispatcher = UnconfinedTestDispatcher()
 ) : DispatcherProvider {
